73372f4f8b692bc04a6605e3c54e839f99d23049
irynasomova <40399278+irynasomova@users.noreply.github.com>
2018-07-02 23:23:31 +0300

    made some fix

--- homework_22.06/hw24.js
+++ homework_22.06/hw24.js
@@ -29,8 +29,8 @@ alert('У Вас ' + plus + ' правильных ответа!');*/
 //TASK 2 про самолет
 
 //данные, которые вводит пользователь
-var value_bak = parseInt(prompt("Enter the value of bak: "));
-var value_initial = parseInt(prompt("Enter the initial value of bak: "));
+var value_tank = parseInt(prompt("Enter the value of tank: "));
+var value_initial = parseInt(prompt("Enter the initial value of tank: "));
 var weight_cargo = parseInt(prompt("Enter the weight of cargo: "));
 var distance_a = parseInt(prompt("Enter the distance from point A to point C: "));
 var distance_b = parseInt(prompt("Enter the distance from point C to point B: "));
@@ -39,7 +39,7 @@ var distance_b = parseInt(prompt("Enter the distance from point C to point B: ")
 var max_value = 500;
 
 //если объем бака, который вводит пользователь, больше макс.объема
-if (value_bak>max_value) alert ("Invalid input");
+if (value_tank>max_value) alert ("Invalid input");
 
 //если груз превышает допустимый вес
 if (weight_cargo>=5) alert("Sorry, the weight is too large, you couldn't fly");
@@ -51,9 +51,9 @@ if (weight_cargo<5) {
 	var rashod_fuel_5 = rashod_fuel_on_100km_5*AB_5; //вычисление нужного объема топлива при заданном расстоянии
 	
 	if (value_initial>=rashod_fuel_5) alert ("You have enough fuel for the flight. Nice trip!"); // в случае, если нужного объема хватит
-	else (value_initial<rashod_fuel_5) { //вычисление недостающего объема топлива
+	else if (value_initial<rashod_fuel_5) { //вычисление недостающего объема топлива
 		var rest_fuel_5 = rashod_fuel_5 - value_initial;
-		alert("your plane will not reach. You need "+rest_fuel_5+" litrs for refueling");
+		alert("your plane will not reach. You need "+rest_fuel_5+" litrs for refueling. But your fuel tank does not have enough place for it! We don't recommend to fly");
 	}
 }
 //при весе груза от 3 до 4 т
@@ -63,9 +63,9 @@ if (weight_cargo<4) {
 	var rashod_fuel_4 = rashod_fuel_on_100km_4*AB_4;
 	
 	if (value_initial>=rashod_fuel_4) alert ("You have enough fuel for the flight. Nice trip!");
-	else (value_initial<rashod_fuel_4) {
+	else if (value_initial<rashod_fuel_4) {
 		var rest_fuel_4 = rashod_fuel_4 - value_initial;
-		alert("your plane will not reach. You need "+rest_fuel_4+" litrs for refueling");
+		alert("your plane will not reach. You need "+rest_fuel_4+" litrs for refueling. But your fuel tank does not have enough place for it! We don't recommend to fly");
 	}
 }
 //при весе груза от 2 до 3 т
@@ -75,9 +75,9 @@ if (weight_cargo<3) {
 	var rashod_fuel_3 = rashod_fuel_on_100km_3*AB_3;
 	
 	if (value_initial>=rashod_fuel_3) alert ("You have enough fuel for the flight. Nice trip!");
-	else (value_initial<rashod_fuel_3) {
+	else if (value_initial<rashod_fuel_3) {
 		var rest_fuel_3 = rashod_fuel_3 - value_initial;
-		alert("your plane will not reach. You need "+rest_fuel_3+" litrs for refueling");
+		alert("your plane will not reach. You need "+rest_fuel_3+" litrs for refueling. But your fuel tank does not have enough place for it! We don't recommend to fly");
 	}
 }
 //при весе груза от 1 до 2 т
@@ -87,9 +87,9 @@ if (weight_cargo<2) {
 	var rashod_fuel_2 = rashod_fuel_on_100km_2*AB_2;
 	
 	if (value_initial>=rashod_fuel_2) alert ("You have enough fuel for the flight. Nice trip!");
-	else (value_initial<rashod_fuel_2) {
+	else if (value_initial<rashod_fuel_2) {
 		var rest_fuel_2 = rashod_fuel_2 - value_initial;
-		alert("your plane will not reach. You need "+rest_fuel_2+" litrs for refueling");
+		alert("your plane will not reach. You need "+rest_fuel_2+" litrs for refueling. But your fuel tank does not have enough place for it! We don't recommend to fly");
 	}
 }
 //при весе груза до 1 т
@@ -99,9 +99,9 @@ if (weight_cargo<1) {
 	var rashod_fuel_1 = rashod_fuel_on_100km_1*AB_1;
 	
 	if (value_initial>=rashod_fuel_1) alert ("You have enough fuel for the flight. Nice trip!");
-	else (value_initial<rashod_fuel_1) {
+	else if (value_initial<rashod_fuel_1) {
 		var rest_fuel_1 = rashod_fuel_1 - value_initial;
-		alert("your plane will not reach. You need "+rest_fuel_1+" litrs for refueling");
+		alert("your plane will not reach. You need "+rest_fuel_1+" litrs for refueling. But your fuel tank does not have enough place for it! We don't recommend to fly");
 	}
 }
 
