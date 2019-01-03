56b64dff2a02e316505993ba11bfb5294694aa3e
irynasomova <40399278+irynasomova@users.noreply.github.com>
2019-01-03 16:09:15 +0200

    Не доделан еще... Kонсольный вариант приложения заметки с получением заметок из http://pdfstep.zzz.com.ua

--- HW_JAVA/connection/src/main/java/org/itstep/connection/Main.java
+++ HW_JAVA/connection/src/main/java/org/itstep/connection/Main.java
@@ -1,7 +1,17 @@
 package org.itstep.connection;
 
+import org.itstep.connection.View.NotesView;
+import org.itstep.connection.View.NotesViewCmd;
+import org.itstep.connection.dao.NotesDao;
+import org.itstep.connection.dao.NotesDaoImpl;
+import org.itstep.connection.presenters.NotesPresenter;
+
 public class Main {
     public static void main(String[] args) {
 
+        NotesView view = new NotesViewCmd();
+        NotesDao dao = new NotesDaoImpl();
+        NotesPresenter presenter = new NotesPresenter(view, dao);
+        presenter.run();
     }
 }
